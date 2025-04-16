b69143c8f66e9a7262388257c6120be48c8a2480
Brooke Gabler <brookeamara3@gmail.com>
2025-04-16 13:41:41 -0600

    people file

---
+++ people/js/scripts.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/small.css">
+  <link rel="stylesheet" href="css/medium.css">
+  <link rel="stylesheet" href="css/large.css">
+  <title>Star Wars Characters</title>
+</head>
+<body>
+  <header>
+    <div class="introText">
+      <h1>Star Wars Characters</h1>
+      <p>Click the buttons to filter as you please.</p>
+    </div>
+    <div class="filterButtons"></div>
+  </header>
+  <main>
+    <div class="characterDiv"></div>
+  </main>
+  <script src="js/scripts.js" type="module"></script>
+</body>
+</html>
