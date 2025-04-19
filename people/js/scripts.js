37160a16fc90563a1303136029e59832cc1299c9
Brooke Gabler <brookeamara3@gmail.com>
2025-04-18 21:40:32 -0600

    senators assignment

--- people/js/scripts.js
+++ people/js/scripts.js
@@ -1,50 +1,63 @@
 
-import{ people } from '../data/people.js'
+import { people } from '../data/people.js'
 console.log(people)
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/small.css">
-  <link rel="stylesheet" href="css/medium.css">
-  <link rel="stylesheet" href="css/large.css">
-  <title>Star Wars Characters</title>
-</head>
-<body>
-  <header>
-    <div class="introText">
-      <h1>Star Wars Characters</h1>
-      <p>Click the buttons to filter as you please.</p>
-    </div>
-    <div class="filterButtons"></div>
-  </header>
-  <main>
-    <div class="characterDiv"></div>
-  </main>
-  <script src="js/scripts.js" type="module"></script>
-</body>
-</html>
-
-
-const myNavigation = document.querySelector('nav')
+const myNavigation = document.querySelector('.nav')
 const myParent = document.querySelector('#peopleHere')
 
-function displayPeople (x) {
+//normal button
+const btnAll = document.createElement("button")
+btnAll.textContent = "Show All"
+btnAll.addEventListener("click",() => displayPeople(people))
+
+
+
+//female button
+const btnFemale = document.createElement("button")
+btnFemale.textContent = "Females"
+btnFemale.addEventListener("click",() => {
+  const arrayFemale = people.filter(person => person.gender === 'female')
+  displayPeople(arrayFemale)
+})
+
+//male button
+const btnMale = document.createElement("button")
+btnMale.textContent = "Males"
+btnMale.addEventListener("click",() => {
+  const arrayMale = people.filter(person => person.gender === 'male')
+  displayPeople(arrayMale)
+})
+
+//Other button
+const btnOther = document.createElement("button")
+btnOther.textContent = "Other"
+btnOther.addEventListener("click",() => {
+  const arrayOther = people.filter(person => person.gender != 'male' && person.gender != 'female')
+  displayPeople(arrayOther)
+})
+
+
+myNavigation.appendChild(btnAll)
+myNavigation.appendChild(btnFemale)
+myNavigation.appendChild(btnMale)
+myNavigation.appendChild(btnOther)
+
+
+//append the female, male, other buttons
+
+
+function displayPeople(x) {
 
   x.forEach(person => {
     const myFigure = document.createElement('figure')
 
 
     const myImage = document.createElement('img')
-   //console.log(person.url)
-   const explodedArray = person.url.split('/')
-   //console.log(explodedArray)
-   const charNumber = explodedArray[5]
-   myImage.src='https://starwars.dgmuvu.com/characters/${charNumber}.jpg'
+    //console.log(person.url)
+    const explodedArray = person.url.split('/')
+    //console.log(explodedArray)
+    const charNumber = explodedArray[5]
+    myImage.src = `https://starwars.dgmuvu.com/characters/${charNumber}.jpg`
     myImage.alt = person.name
 
 
@@ -55,14 +68,14 @@ function displayPeople (x) {
     // assign gender class
     console.log(person.gender)
     switch (person.gender) {
-      case: "female";
+      case "female":
 
-       myFigure.className = "female"
-      break;
-      case: "male":
+        myFigure.className = "female"
+        break;
+      case "male":
         myFigure.className = "male"
-      break;
-      default;
+        break;
+      default:
         myFigure.className = "other"
 
     } //end of switch
@@ -74,7 +87,7 @@ function displayPeople (x) {
     myFigure.appendChild(myCaption)
 
 
-    myParent.appendChild
+    myParent.appendChild(myFigure)
 
   }
 
