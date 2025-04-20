64af4dac23009e5369b6f726e60d7b84cf9943b4
Brooke Gabler <brookeamara3@gmail.com>
2025-04-19 22:04:14 -0600

    fixing css in senators

--- senators/js/scripts.js
+++ senators/js/scripts.js
@@ -1,72 +1,120 @@
-import { senators } from '../data/senators.js';
-console.log(senators);
 
-// HTML element references
-const myNavigation = document.querySelector('.nav');
-const cardParent = document.querySelector('#allcards');
+import {allSenators} from '../data/senators.js'
+console.log(allSenators);
 
-// Button: All Senators
-const btnAll = document.createElement("button");
-btnAll.textContent = "All Senators";
-btnAll.addEventListener('click', () => displaySenator(senators));
+//grab references to the HTML stuff
+const myNavigation = document.querySelector('nav');
+const cardParent = document.querySelector('#allCards');
 
-// Button: Female Senators
-const btnFemale = document.createElement("button");
-btnFemale.textContent = "Females";
-btnFemale.addEventListener("click", () => {
-  const arrayFemale = senators.filter(senator => senator.gender === 'female');
-  displaySenator(arrayFemale);
+//create an all senators button 
+const btnAll = document.createElement('button');
+btnAll.textContent = 'All'
+btnAll.addEventListener('click', () => {
+   displayPeople(allSenators)
+})
+
+
+//Event Listener and filter for the Democrats
+const btnDemocrats = document.createElement('button');
+btnDemocrats.textContent = 'Democrats'
+btnDemocrats.addEventListener('click', () => {
+   const democratSenators =allSenators.filter(x => x.party === 'D')
+   displayPeople(democratSenators)
+})//end of button
+
+
+
+//Event Listener and filter for the Republicans
+const btnRepublicans = document.createElement('button');
+btnRepublicans.textContent = 'Republicans'
+btnRepublicans.addEventListener('click', () => {
+   const republicansSenators =allSenators.filter(x => x.party === 'R')
+   displayPeople(republicansSenators)
+})// end of republican button
+
+
+
+//Event Listener and filter for the women
+const btnFemale = document.createElement('button');
+btnFemale.textContent = 'Women'
+btnFemale.addEventListener('click', () => {
+   const femaleSenators =allSenators.filter(x => x.gender === 'F')
+   displayPeople(femaleSenators)
+});// end of female
+
+
+
+//Event Listener and filter for the men
+const btnMale = document.createElement('button');
+btnMale.textContent = 'Man'
+btnMale.addEventListener('click', () => {
+   const maleSenators =allSenators.filter(x => x.gender === 'M')
+   displayPeople(maleSenators)
 });
 
-// Button: Male Senators
-const btnMale = document.createElement("button");
-btnMale.textContent = "Males";
-btnMale.addEventListener("click", () => {
-  const arrayMale = senators.filter(senator => senator.gender === 'male');
-  displaySenator(arrayMale);
-});
 
-// Add buttons to the navigation
-myNavigation.appendChild(btnAll);
-myNavigation.appendChild(btnFemale);
-myNavigation.appendChild(btnMale);
+//add buttons to page 
+myNavigation.appendChild(btnAll)
+myNavigation.appendChild(btnDemocrats)
+myNavigation.appendChild(btnRepublicans)
+myNavigation.appendChild(btnFemale)
+myNavigation.appendChild(btnMale)
 
-// Function to display senators
-function displaySenator(senatorArray) {
-  cardParent.innerHTML = ""; // Clear current cards
+//loop through all the people 
+function displayPeople (x) {
+    cardParent.textContent = ""
 
-  senatorArray.forEach(senator => {
-    const myFigure = document.createElement('figure');
 
-    // Create Image
-    const explodedArray = senator.url.split('/');
-    const charNumber = explodedArray[5];
-    const myImage = document.createElement('img');
-    myImage.src = `https://www.govtrack.us/static/legislator-photos/${charNumber}-200px.jpeg`;
-    myImage.alt = senator.name;
+    x.forEach(senator => {
+        const myFigure = document.createElement('figure')
 
-    // Create Caption
-    const myCaption = document.createElement('figcaption');
-    myCaption.textContent = senator.name;
+        let barDiv = document.createElement('div')
+        barDiv.innerHTML = `<p>${senator.first_name}<br>${senator.last_name}</p>`
+        console.log(barDiv)
 
-    // Gender class
+        const myCaption = document.createElement('figcaption')
+        myCaption.innerHTML = `${senator.state}<br>${senator.date_of_birth}<br>${senator.state_rank}<br>Party: ${senator.party}`
+        //(senator.first_name) + (senator.last_name)//need to have space between first and last 
+
+
+        const myImage = document.createElement('img')
+        myImage.src=`https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`
+
+    //asign gender class
+    console.log(senator.gender)
     switch (senator.gender) {
-      case "female":
-        myFigure.className = "female";
+        case "F":
+            myFigure.className = "Female"
         break;
-      case "male":
-        myFigure.className = "male";
+        case "M":
+            myFigure.className = "Male"
         break;
-      default:
-        myFigure.className = "other";
     }
+        switch (senator.party) {
+            case "D":
+                myFigure.className = "Democrat"
+            break;
+            case "R":
+                myFigure.className = "Republican"
+            break;
+            default:
+                myFigure.className = "other"
+        }
 
-    // Assemble figure
-    myFigure.appendChild(myImage);
-    myFigure.appendChild(myCaption);
-    cardParent.appendChild(myFigure);
-  });
-}
 
-// Display all on page load
-displaySenator(senators);
+        // assemble the parts
+        myFigure.appendChild(barDiv)
+        myFigure.appendChild(myImage)
+        myFigure.appendChild(myCaption)
+
+        // attach to the html page
+        cardParent.appendChild(myFigure)
+        
+        
+
+    }// end of fat arrow
+    )//end of loop
+} //end of function
+
+//call the function
+displayPeople(allSenators);
